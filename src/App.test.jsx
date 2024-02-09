cb488258a2777a8eb4ae19bcf041a1b165478115
Fernando Dodino <fernando.dodino@gmail.com>
2024-02-08 22:32:11 -0300

    Fix test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { prestamoService } from './services/prestamoService'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Préstamos/i)
-  expect(linkElement).toBeInTheDocument()
+describe('App', () => {
+
+  beforeEach(() => {
+    vi.spyOn(prestamoService, 'getPrestamosPendientes').mockImplementation(() => [])
+  })
+
+  test('renders learn react link', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/Préstamos/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
 })
